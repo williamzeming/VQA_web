99803ec705c92392dbba4d8fa6e19e9dac5f582d
williamzeming <zren6825@uni.sydney.edu.au>
2023-09-20 01:28:20 +1000

    Alert for upload success or error message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,25 +4,65 @@ import Grid from '@material-ui/core/Grid';
 import PDFUploader from "./PDFUploader";
 import ChatBox from "./ChatBox";
 import simple from './simple_img.png'
-const hidden_input = {
-    display: 'none',
-};
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import ImageIcon from '@material-ui/icons/Image';
+import WorkIcon from '@material-ui/icons/Work';
+import BeachAccessIcon from '@material-ui/icons/BeachAccess';
+
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 <h1 align='center'>VQA is Query Application!</h1>
-                <div align='left'>
-                    <img src={simple} height={"500px"}/>
-                </div>
-                <div align='left'>
-                    <ChatBox />
-                </div>
-                <div align='left'>
-                    <PDFUploader />
-                </div>
+                <Grid container>
+                    <Grid item xs={2}>
+                        <List>
+                            <ListItem button>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <ImageIcon/>
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary="Photos" secondary="Jan 9, 2014"/>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <WorkIcon/>
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary="Work" secondary="Jan 7, 2014"/>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <BeachAccessIcon/>
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary="Vacation" secondary="July 20, 2014"/>
+                            </ListItem>
+                        </List>
 
+
+                    </Grid>
+                    <Grid item xs={10}>
+                        <div align='left'>
+                            <img src={simple} height={"500px"}/>
+                        </div>
+                        <div align='left'>
+                            <ChatBox/>
+                        </div>
+                        <div align='left'>
+                            <PDFUploader/>
+                        </div>
+                    </Grid>
+                </Grid>
             </div>
 
 
--- frontend/src/PDFUploader.js
+++ frontend/src/PDFUploader.js
@@ -1,61 +1,122 @@
 import React from 'react';
 import {Input, Button, IconButton} from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
 
+import Box from '@mui/material/Box';
+import Alert from '@mui/material/Alert';
+import Collapse from '@mui/material/Collapse';
+import CloseIcon from '@mui/icons-material/Close';
+import {Typography} from "@mui/material";
+
 const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+        },
     },
-  },
-  input: {
-    display: 'none',
-  },
+    input: {
+        display: 'none',
+    },
 }));
 
 
 function PDFUploader() {
+    const [s_open, sets_Open] = React.useState(false);
+    const [e_open, sete_Open] = React.useState(false);
     const classes = useStyles()
     const handleUpload = async (event) => {
-    event.preventDefault();
-    const formData = new FormData();
-    const fileInput = document.getElementById("pdfInput");
-    const file = fileInput.files[0];
-    formData.append("file", file);
+        event.preventDefault();
+        const formData = new FormData();
+        const fileInput = document.getElementById("pdfInput");
+        const file = fileInput.files[0];
+        formData.append("file", file);
 
-    try {
-        const response = await fetch("http://localhost:8988/upload", {
-            method: "POST",
-            body: formData,
-        });
+        try {
+            const response = await fetch("http://localhost:8988/upload", {
+                method: "POST",
+                body: formData,
+            });
 
-        const data = await response.json();
-        console.log(data);
+            const data = await response.json();
+            console.log(data);
+            if ("error" in data) {
+                sete_Open(true)
+            } else {
+                sets_Open(true)
+            }
+        } catch (error) {
+            console.error("Upload error", error);
 
-    } catch (error) {
-        console.error("Upload error", error);
-    }
-};
+        }
+    };
 
 
     return (
         <div>
-    <h2>Upload your PDF</h2>
-    <div>
-        {/*<Input type="text" style={{ marginRight: '10px',width:'500px' }}></Input>*/}
-
-        <form onSubmit={handleUpload}>
-            <label htmlFor="pdfInput">
-                <IconButton color="secondary" aria-label="upload picture" component="span">
-                    <AttachFileIcon />
-                </IconButton>
-            </label>
-            <input type="file" accept="application/pdf" id="pdfInput" className={classes.input} color="primary"/>
-            <Button type="submit" variant="contained" color="primary">upload</Button>
-        </form>
-    </div>
-</div>
+            <h2>Upload your PDF</h2>
+            <div>
+                <form onSubmit={handleUpload}>
+                    <label htmlFor="pdfInput">
+                        <IconButton color="secondary" aria-label="upload picture" component="span">
+                            <AttachFileIcon/>
+                        </IconButton>
+                    </label>
+                    <input type="file" accept="application/pdf" id="pdfInput" className={classes.input}
+                           color="primary"/>
+                    <Button type="submit" variant="contained" color="primary">upload</Button>
+                </form>
+            </div>
+            <div>
+                <Box sx={{width: '50%'}}
+                     justifyContent="center"
+                     alignItems="center"
+                     flexDirection="column">
+                    <Collapse in={s_open}>
+                        <Alert severity="success"
+                               action={
+                                   <IconButton
+                                       aria-label="close"
+                                       color="inherit"
+                                       size="medium"
+                                       onClick={() => {
+                                           sets_Open(false);
+                                       }}
+                                   >
+                                       <CloseIcon fontSize="inherit"/>
+                                   </IconButton>
+                               }
+                               sx={{mb: 2}}
+                        >
+                            <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                                File uploaded successfully
+                            </Typography>
+                        </Alert>
+                    </Collapse>
+                    <Collapse in={e_open}>
+                        <Alert severity="error"
+                               action={
+                                   <IconButton
+                                       aria-label="close"
+                                       color="inherit"
+                                       size="medium"
+                                       onClick={() => {
+                                           sete_Open(false);
+                                       }}
+                                   >
+                                       <CloseIcon fontSize="inherit"/>
+                                   </IconButton>
+                               }
+                               sx={{mb: 2}}
+                        >
+                            <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                                File uploaded error
+                            </Typography>
+                        </Alert>
+                    </Collapse>
+                </Box>
+            </div>
+        </div>
 
     );
 }
