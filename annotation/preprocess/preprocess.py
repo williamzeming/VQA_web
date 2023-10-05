a51edf2052b28af91e8ff93af719352bba35c051
rxw99 <122262643+rxw99@users.noreply.github.com>
2023-10-06 01:15:06 +1100

    update annotation preprocess function

--- annotation/preprocess/preprocess.py
+++ annotation/preprocess/preprocess.py
@@ -31,92 +31,93 @@ def pdftoimg(filepath, imgpath):
         pix.save(os.path.join(imgpath, img_name))
 
 
-def extract_text(file_path, file_list, img_without_bbox, img_with_bbox, img_textlines):
+def preprocess(doc_path):
     """
     Extract text line information from PDF.
-
-    file_path: folder path that store the PDF files
-    file_list: list of PDF files
-    img_without_bbox: folder path that store the image without bounding box
-    img_with_bbox: folder path that store the image with bounding box
-    img_textlies: folder path that sore the textline json file for each images
+    doc_path: path of pdf that need to preprocess
     """
+    img_textlines = './img_textline/'
+    img_with_bbox = './img_with_bbox/'
+    img_without_bbox = './img_with_bbox/'
+
     count = 0
-    error_pages = {}
-    for todo_file in tqdm(file_list):
-        count += 1
+    # error_pages = {}
+    count += 1
 
-        doc_path = file_path + todo_file
-        file_name = todo_file[:-4]
-        pdftoimg(doc_path, img_without_bbox)
+    todo_file = doc_path.split('/')[-1]
+    file_name = todo_file[:-4]
+    pdftoimg(doc_path, img_without_bbox)
 
-        document = open(doc_path, 'rb')
-        # Create resource manager
-        rsrcmgr = PDFResourceManager()
-        # Set parameters for analysis.
-        laparams = LAParams()
-        # Create a PDF page aggregator object.
-        device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        page_num = 0
+    document = open(doc_path, 'rb')
+    # Create resource manager
+    rsrcmgr = PDFResourceManager()
+    # Set parameters for analysis.
+    laparams = LAParams()
+    # Create a PDF page aggregator object.
+    device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    page_num = 0
 
-        print("Now processing : ", count)
-        try:
-            for page in PDFPage.get_pages(document):
+    # print("Now processing : ", count)
+    try:
+        for page in PDFPage.get_pages(document):
 
-                json_file = {file_name: {}}
+            json_file = {file_name: {}}
 
-                interpreter.process_page(page)
-                # receive the LTPage object for the page.
-                layout = device.get_result()
-                obj_id = 0
+            interpreter.process_page(page)
+            # receive the LTPage object for the page.
+            layout = device.get_result()
+            obj_id = 0
 
-                imgFilePath = img_without_bbox + file_name + '_page-' + str(page_num) + '.png'
+            imgFilePath = img_without_bbox + file_name + '_page-' + str(page_num) + '.png'
 
-                img = cv2.imread(imgFilePath, cv2.IMREAD_UNCHANGED)
+            img = cv2.imread(imgFilePath, cv2.IMREAD_UNCHANGED)
 
-                imgHeight, imgWidth, imgChannels = img.shape
+            imgHeight, imgWidth, imgChannels = img.shape
 
-                pageW = page.mediabox[2]
-                pageH = page.mediabox[3]
-                sizeRatioW = imgWidth / page.mediabox[2]
-                sizeRatioH = imgHeight / page.mediabox[3]
+            pageW = page.mediabox[2]
+            pageH = page.mediabox[3]
+            sizeRatioW = imgWidth / page.mediabox[2]
+            sizeRatioH = imgHeight / page.mediabox[3]
 
-                for lt_obj in layout:
-                    if isinstance(lt_obj, LTTextBox):
-                        for obj in lt_obj:
-                            new_bbox = get_bbox(sizeRatioW, sizeRatioH, pageW, pageH, obj.bbox)  # find text box
-                            cv2.rectangle(img, (new_bbox[0], new_bbox[1]), (new_bbox[2], new_bbox[3]), (0, 0, 255, 255),
-                                          1)  # red
-                            if obj_id not in json_file[file_name]:
-                                json_file[file_name][obj_id] = {}
-                                json_file[file_name][obj_id]['LTTextBox'] = {}
-                                json_file[file_name][obj_id]['LTTextBox']['bbox'] = new_bbox
-                                json_file[file_name][obj_id]['LTTextBox']['text'] = obj.get_text()
-                                obj_id += 1
+            for lt_obj in layout:
+                if isinstance(lt_obj, LTTextBox):
+                    for obj in lt_obj:
+                        new_bbox = get_bbox(sizeRatioW, sizeRatioH, pageW, pageH, obj.bbox)  # find text box
+                        cv2.rectangle(img, (new_bbox[0], new_bbox[1]), (new_bbox[2], new_bbox[3]), (0, 0, 255, 255),
+                                      1)  # red
+                        if obj_id not in json_file[file_name]:
+                            json_file[file_name][obj_id] = {}
+                            json_file[file_name][obj_id]['LTTextBox'] = {}
+                            json_file[file_name][obj_id]['LTTextBox']['bbox'] = new_bbox
+                            json_file[file_name][obj_id]['LTTextBox']['text'] = obj.get_text()
+                            obj_id += 1
 
-                    elif isinstance(lt_obj, LTFigure):
-                        for obj in lt_obj:
-                            new_bbox = get_bbox(sizeRatioW, sizeRatioH, pageW, pageH, obj.bbox)  # find text box
-                            cv2.rectangle(img, (new_bbox[0], new_bbox[1]), (new_bbox[2], new_bbox[3]), (0, 255, 0, 255),
-                                          1)  # green
-                            if obj_id not in json_file[file_name]:
-                                json_file[file_name][obj_id] = {}
-                                json_file[file_name][obj_id]['LTFigure'] = {}
-                                json_file[file_name][obj_id]['LTFigure']['bbox'] = new_bbox
-                                obj_id += 1
+                elif isinstance(lt_obj, LTFigure):
+                    for obj in lt_obj:
+                        new_bbox = get_bbox(sizeRatioW, sizeRatioH, pageW, pageH, obj.bbox)  # find text box
+                        cv2.rectangle(img, (new_bbox[0], new_bbox[1]), (new_bbox[2], new_bbox[3]), (0, 255, 0, 255),
+                                      1)  # green
+                        if obj_id not in json_file[file_name]:
+                            json_file[file_name][obj_id] = {}
+                            json_file[file_name][obj_id]['LTFigure'] = {}
+                            json_file[file_name][obj_id]['LTFigure']['bbox'] = new_bbox
+                            obj_id += 1
 
-                # save img
-                img_file = img_with_bbox + file_name + '_page-' + str(page_num) + '.png'
-                cv2.imwrite(img_file, img)
+            # save img
+            img_file = img_with_bbox + file_name + '_page-' + str(page_num) + '.png'
+            cv2.imwrite(img_file, img)
 
-                file_loc = img_textlines + file_name + "_page-" + str(page_num) + ".json"
+            file_loc = img_textlines + file_name + "_page-" + str(page_num) + ".json"
 
-                with open(file_loc, 'w') as output:
-                    json_str = json.dumps(json_file)
-                    output.write(json_str)
+            with open(file_loc, 'w') as output:
+                json_str = json.dumps(json_file)
+                output.write(json_str)
 
-                page_num += 1
-        except Exception as e:
-            error_pages[todo_file] = str(e)
-        return error_pages
+            page_num += 1
+    except Exception as e:
+        print(f'The file {todo_file} cannot extract textline information. Please try another file.')
+        # error_pages[todo_file] = str(e)
+    # return error_pages
+
+preprocess('/Users/ruxiw/PycharmProjects/VQA_web/uploads/PDF-VQA.pdf')
